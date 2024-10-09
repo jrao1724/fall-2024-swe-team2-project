31de104fc0a2cc910f31367e0f4507c817c41b19
Jayanth Rao <jrao1724@gmail.com>
2024-10-09 15:51:51 -0400

    migrated database from sqlite3 to postgres

--- campus_grocery_app/settings.py
+++ campus_grocery_app/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 from datetime import timedelta
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -88,10 +91,21 @@ WSGI_APPLICATION = 'campus_grocery_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
--- marketplace/models.py
+++ marketplace/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+# class Post(models.Model):
+#     POST_TYPE = (
+#         ('buying', 'Buying'),
+#         ('selling', 'Selling')
+#     )
+
+#     post_id = models.AutoField(primary_key=True)
+#     user_location = models
