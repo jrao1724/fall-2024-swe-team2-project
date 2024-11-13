16a88a12efcdeac059ad5561028a1ec3101b760d
Jayanth Rao <jrao1724@gmail.com>
2024-11-13 11:14:40 -0500

    added fields to recipe model

--- recipes/models.py
+++ recipes/models.py
@@ -22,10 +22,16 @@ class Recipe(models.Model):
         ('hard', 'Hard')
     )
 
+    TIME_UNITS = (
+        ('mins', 'Minutes'),
+        ('hrs', 'Hours')
+    )
+
     recipe_id = models.AutoField(primary_key=True)
     recipe_name = models.CharField(max_length=255)
     difficulty_level = models.CharField(max_length=10, choices=DIFFICULTY_CHOICES, default='easy')
     quickness = models.PositiveIntegerField(help_text="Time in minutes")
+    time_unit = models.CharField(max_length=4, choices=TIME_UNITS, default='mins')
     nutrition = models.JSONField()
     
     ingredients = models.ManyToManyField('Ingredient', through='RecipeIngredient')
@@ -36,6 +42,7 @@ class Recipe(models.Model):
     rating = models.DecimalField(max_digits=2, decimal_places=1, default=0.0)
     image = models.ImageField(upload_to='recipe_images/', blank=True, null=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.TextField()
 
     def __str__(self):
         return str(self.recipe_name)
