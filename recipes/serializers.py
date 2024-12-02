38c9dfbe02a27787ae39ec904955049fe18bd472
Jayanth Rao <jayanth.rao@hpe.com>
2024-12-02 18:43:16 -0500

    added functionality for saving recipes, updating recipe ratings, and searching recipes through allergens

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from .models import Recipe, Allergen, DietaryRestriction
+from .models import Recipe, Allergen, DietaryRestriction, Rating
+from user_accounts.models import User
 
 class RecipeSerializer(serializers.ModelSerializer):
     allergens = serializers.StringRelatedField(many=True)
@@ -35,3 +36,59 @@ class RecipeSerializer(serializers.ModelSerializer):
         parsed_user = self.context['request'].user
         recipe = Recipe.objects.create(created_by=parsed_user, **validated_data)
         return recipe
+    
+
+class RatingSerializer(serializers.ModelSerializer):
+    recipe_id = serializers.IntegerField(write_only=True)  # Input recipe ID
+    rating = serializers.IntegerField(min_value=1, max_value=5)  # Ensure rating is between 1 and 5
+    user = serializers.StringRelatedField(read_only=True)
+
+    class Meta:
+        model = Rating
+        fields = ['id', 'recipe_id', 'rating', 'user']
+
+    def validate_recipe_id(self, value):
+        from .models import Recipe
+        try:
+            Recipe.objects.get(id=value)
+        except Recipe.DoesNotExist:
+            raise serializers.ValidationError("Recipe with this ID does not exist.")
+        return value
+
+    def create(self, validated_data):
+        recipe_id = validated_data.pop('recipe_id')
+        user = self.context['request'].user
+        from .models import Recipe
+        recipe = Recipe.objects.get(id=recipe_id)
+        rating, created = Rating.objects.update_or_create(
+            user=user, recipe=recipe, defaults={'rating': validated_data['rating']}
+        )
+        recipe.update_average_rating()  # Ensure the recipe's average rating is updated
+        return rating
+
+    def update(self, instance, validated_data):
+        instance.rating = validated_data.get('rating', instance.rating)
+        instance.save()
+        instance.recipe.update_average_rating()
+        return instance
+    
+
+class SaveRecipeSerializer(serializers.ModelSerializer):
+    recipe_id = serializers.IntegerField(write_only=True)  # Input recipe ID
+
+    class Meta:
+        model = User
+        fields = ['recipe_id']
+
+    def validate_recipe_id(self, value):
+        try:
+            Recipe.objects.get(id=value)
+        except Recipe.DoesNotExist:
+            raise serializers.ValidationError("Recipe with this ID does not exist.")
+        return value
+
+    def update(self, instance, validated_data):
+        recipe_id = validated_data.get('recipe_id')
+        recipe = Recipe.objects.get(id=recipe_id)
+        instance.saved_recipes.add(recipe)
+        return instance
--- recipes/urls.py
+++ recipes/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import RecipeListCreateView, RecipeDetailView, RecipeCheckExistsView, RecipeFilterByRestrictionView
+from .views import RecipeListCreateView, RecipeDetailView, RecipeCheckExistsView, RecipeSearchFilterAPIView, RatingView, SaveRecipeByUserView
 
 urlpatterns = [
     path('recipes/addRecipe/', RecipeListCreateView.as_view(), name='recipe-list-create'),
     path('recipes/recipeInfo/<int:pk>/', RecipeDetailView.as_view(), name='recipe-detail'),
     path('recipes/exists/<str:name>/', RecipeCheckExistsView.as_view(), name='recipe-check-exists'),
-    path('recipes/filter/', RecipeFilterByRestrictionView.as_view(), name='recipe-filter-by-restriction')
+    path('recipes/filter/', RecipeSearchFilterAPIView.as_view(), name='recipe-filter-by-restriction'),
+    path('recipes/rateRecipe/', RatingView.as_view(), name='rate-recipe'), # POST
+    path('recipes/saveRecipe/', SaveRecipeByUserView.as_view(), name='save-recipe') # POST
 ]
