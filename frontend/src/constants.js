037ded9075cedd52e81acc80546d5dd470323d34
Disha Anil <dishaanil@dishas-laptop.mynetworksettings.com>
2024-12-17 11:04:18 -0500

    Added production backend path

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,3 +1,6 @@
-const API_BASE_URL = 'http://127.0.0.1:8000';
+const API_BASE_URL =
+  process.env.NODE_ENV === 'production'
+    ? 'https://campus-cuisine-backend-f8fjfcd2agdacqef.eastus2-01.azurewebsites.net'
+    : 'http://127.0.0.1:8000';
 
 export default API_BASE_URL;
