d0962cf2fd1a951b5ec40670ab89d0a4950e852b
Disha Anil <dishaanil@dishas-laptop.mynetworksettings.com>
2024-12-17 11:34:21 -0500

    fixed routing

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,6 +1,8 @@
-const API_BASE_URL =
-  process.env.NODE_ENV === 'production'
-    ? 'https://campus-cuisine-backend-f8fjfcd2agdacqef.eastus2-01.azurewebsites.net'
-    : 'http://127.0.0.1:8000';
+//const API_BASE_URL =
+  //process.env.NODE_ENV === 'production'
+    //? 'https://campus-cuisine-backend-f8fjfcd2agdacqef.eastus2-01.azurewebsites.net'
+    //: 'http://127.0.0.1:8000';
+const API_BASE_URL = '/apis/rest';
+export default API_BASE_URL; 
 
-export default API_BASE_URL;
+//export default API_BASE_URL;
